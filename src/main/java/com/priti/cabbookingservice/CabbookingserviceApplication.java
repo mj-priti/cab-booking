9d625e8098a9d4d2d340afb3e189d5671fa999da
priti-verma-git <arpit.priti@gmail.com>
2025-02-09 11:04:34 +0530

    initial commit
    Base folder

---
+++ src/main/java/com/priti/cabbookingservice/CabbookingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.priti.cabbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabbookingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabbookingserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priti/cabbookingservice/CabbookingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priti.cabbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabbookingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
